9d57c2c36a3b0c562c7a890066b7cb38af4aba55
Rajat Verma <rajat98v@gmail.com>
2020-06-06 20:15:59 +0530

    User registration added

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -5,7 +5,8 @@ app_name = 'accounts'
 
 urlpatterns = [
         path('', views.index_view, name='index_view'),
-        path('logout_view', views.logout_view, name='logout_view'),
-        path('login_view', views.login_view, name='login_view' ),
-        path('loginme', views.loginme, name='loginme'),
+        path('logout_view/', views.logout_view, name='logout_view'),
+        path('login_view/', views.login_view, name='login_view' ),
+        path('loginme/', views.loginme, name='loginme'),
+        path('register_view/', views.register_view, name='register_view')
 ]
--- website/accounts/views.py
+++ website/accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, auth
+from django.contrib import messages 
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -22,3 +24,29 @@ def loginme(request):
 
 def index_view(request):
     return render(request, 'accounts/index.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1==password2 :
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('accounts:register_view')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('accounts:register_view')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('accounts:login_view')
+        else :
+            messages.info(request, 'Password Not Maching')
+            return redirect('accounts:register_view')
+    else:
+        return render(request, 'accounts/registration.html')
