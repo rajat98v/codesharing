46535869039b987cb64b947b4624c054d8443657
includesid <depanshuiit2017@gmail.com>
2020-06-07 16:28:56 +0530

    ...

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 app_name = 'accounts'
@@ -8,5 +8,6 @@ urlpatterns = [
         path('logout_view/', views.logout_view, name='logout_view'),
         path('login_view/', views.login_view, name='login_view' ),
         path('loginme/', views.loginme, name='loginme'),
-        path('register_view/', views.register_view, name='register_view')
+        path('register_view/', views.register_view, name='register_view'),
+        path('accounts/',include('allauth.urls')),
 ]
--- website/website/settings.py
+++ website/website/settings.py
@@ -143,3 +143,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL= '/'
